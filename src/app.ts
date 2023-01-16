9db2bae36e12208fa67d8b71174e68e8b3a5f2a1
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-01-16 11:22:07 -0500

    config views file ejs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import path from "path";
+
 import { uploadRouter } from "./routes";
 
 const app = express();
+
+// Settings
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Middlewares
 app.use(morgan("dev"));
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,8 @@ import { Request, Response, Router } from "express";
 
 const router = Router();
 router.get("/", (req: Request, res: Response) => {
-	res.send({ message: "Welcome to the Home page!" });
+	// res.send({ message: "Welcome to the Home page!" });
+	res.render("index");
 });
 
 export { router as uploadRouter };
