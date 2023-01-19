c08731d7ce4dccf3c2ec89fbd7709f7e7c182005
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-01-19 11:57:35 -0500

    clean app

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import expressLayouts from 'express-ejs-layouts';
 
 import { uploadRouter } from './routes';
-import multer from 'multer';
 
 const app = express();
 // Static Files
@@ -16,24 +15,9 @@ app.use(expressLayouts);
 app.set('layout', path.join(__dirname, 'views/layouts/Site'));
 app.set('view engine', 'ejs');
 
-const storage = multer.diskStorage({
-  destination: path.join(__dirname, 'public/uploads'),
-  filename: (req, file: Express.Multer.File, cb) => {
-    const ext = file.originalname.split('.').pop();
-    const fileName2 = `image-${Date.now()}.${ext ?? 'jpg'}`;
-    cb(null, fileName2);
-  }
-});
-
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(
-  multer({
-    storage,
-    dest: path.join(__dirname, 'public/uploads')
-  }).single('file')
-);
 
 // Routes
 app.use('/', uploadRouter);
